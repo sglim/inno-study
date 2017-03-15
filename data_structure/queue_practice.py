ac309c29848fa25d60a29d32e90d492512eab2e8
INO <inoh.jung@gmail.com>
2017-03-15 15:10:43 +0900

    modify queue data array usage as circular one

--- data_structure/queue_practice.py
+++ data_structure/queue_practice.py
@@ -1,94 +1,130 @@
 # queue implementation practice
 
 
-class Queue(object):
+class CircularQueue(object):
 
     def __init__(self):
+        self.capacity = 5  # initial length of data array.
+
+        # initializing data array
         self.data = []
+        for i in range(self.capacity):
+            self.data.append(None)
+
+        self.front = 0
+        self.end = -1
+        self.size = 0
 
     def is_empty(self):
-        if len(self.data) <= 0:
+        if self.size == 0:
+            return True
+        else:
+            return False
+
+    def is_full(self):
+        if self.size == self.capacity:
             return True
         else:
             return False
 
     def check_size(self):
-        return len(self.data)
+        return self.size
 
-    def enqueue(self, value):
-        self.data.insert(0, value)  # use first side of list as rear of queue
-
-    def dequeue(self):
-        if self.is_empty():
-            print("queue is empty")
-            return None
-        else:
-            return self.data.pop()  # use end side of list as front of queue
+    def check_capacity(self):
+        return self.capacity
 
     def print_queue(self):
         print(self.data)
+        print('front :', self.front, '/ end :', self.end)
 
     def front(self):
         if self.is_empty():
             print("queue is empty")
             return None
         else:
-            return self.data[len(self.data)-1]
+            return self.data[self.front]
+
+    # return circular index if input index is larger than maximum index of data array
+    def get_circular_index(self, idx):
+        if idx >= self.capacity:
+            idx %= self.capacity
+        return idx
+
+    # reallocate data array as twice larger one
+    def data_reallocate(self):
+        # initializing new data array
+        new_capacity = 2*self.capacity
+        self.new_data = []
+        for i in range(new_capacity):
+            self.new_data.append(None)
+
+        # copy old data into new data array. rearrange data location so front index shall be zero(0)
+        j = 0
+        while self.front != self.end:
+            self.new_data[j] = self.data[self.front]
+            j += 1
+            self.front += 1
+            self.front = self.get_circular_index(self.front)
+
+        self.new_data[j] = self.data[self.end]  # after while loop, final data (at end index) is not copied yet.
+
+        # reset front & end index, its capacity and data array name
+        self.front = 0
+        self.end = j
+        self.data = self.new_data
+        self.capacity = new_capacity
+
+    def enqueue(self, value):
+        if self.is_full():
+            self.data_reallocate()
+
+        self.size += 1
+        self.end += 1
+        self.end = self.get_circular_index(self.end)  # if increased index is out of maximum, get circular index
+
+        self.data[self.end] = value
+
+    def dequeue(self):
+        if self.is_empty():
+            print("queue is empty")
+            return None
+        else:
+            front_data = self.data[self.front]  # copy data which shall be popped.
+            self.data[self.front] = None  # remove popped data to prevent confusion
+            self.front += 1
+            self.size -= 1
+            self.front = self.get_circular_index(self.front)  # if increased index is out of maximum, get circular index
+            return front_data  # return copied data
 
 
-# queue implementation test
-# uncomment each line to test
+# Circular array queue implementation test
+# uncommend each line to test
+# cq = CircularQueue()
+# print('create Circular Queue instance. check size & capacity : ', cq.check_size(), '&', cq.check_capacity())
+# print('is queue empty? :', cq.is_empty())
+# print('\nenqueue some data : [5,6,8,10,9]')
+# for i in [5, 6, 8, 10, 9]:
+#     cq.enqueue(i)
+# print('is queue full now? :', cq.is_full())
+# cq.print_queue()
 #
-# test_queue = Queue()
+# print('\ndequeue some 3 data now')
+# print('dequeue :', cq.dequeue())
+# print('dequeue :', cq.dequeue())
+# print('dequeue :', cq.dequeue())
+# print('check size & capacity :', cq.check_size(), '&', cq.check_capacity())
+# cq.print_queue()
 #
-# print("queue is empty now? : ",test_queue.is_empty())
-# print('')
+# print('\nenqueue again now : [7,-2,-7]')
+# for i in [7, -2, -7]:
+#     cq.enqueue(i)
+# print('check size & capacity :', cq.check_size(), '&', cq.check_capacity())
+# cq.print_queue()
 #
-# print('enqueue 3 now : ')
-# test_queue.enqueue(3)
-# test_queue.print_queue()
-# print('check size : ', test_queue.check_size())
-# print('front value : ', test_queue.front())
-# print('')
-#
-# print('enqueue 5,-9,0,9,-9,57')
-# for i in [5, -9, 0, 9, -9, 57]:
-#     test_queue.enqueue(i)
-# test_queue.print_queue()
-# print('check size : ', test_queue.check_size())
-# print('front value : ', test_queue.front())
-# print('')
-#
-# print('dequeue : ', test_queue.dequeue())
-# test_queue.print_queue()
-# print('check size : ', test_queue.check_size())
-# print('front value : ', test_queue.front())
-# print('')
-#
-# print('dequeue : ', test_queue.dequeue())
-# print('dequeue : ', test_queue.dequeue())
-# print('dequeue : ', test_queue.dequeue())
-# print('dequeue : ', test_queue.dequeue())
-# print('dequeue : ', test_queue.dequeue())
-# test_queue.print_queue()
-# print('check size : ', test_queue.check_size())
-# print('front value : ', test_queue.front())
-# print('')
-#
-# print('dequeue : ', test_queue.dequeue())
-# test_queue.print_queue()
-# print('check size : ', test_queue.check_size())
-# print('front value : ', test_queue.front())
-# print('')
-#
-# print('dequeue : ', test_queue.dequeue())
-# print('check size : ', test_queue.check_size())
-# print('')
-#
-# print('enqueue 2,8,7,0,-5')
-# for i in [2, 8, 7, 0, -5]:
-#     test_queue.enqueue(i)
-# test_queue.print_queue()
-# print('check size : ', test_queue.check_size())
-# print('front value : ', test_queue.front())
-#
+# print('\nenqueue more. test array reallocation : [12,15,19]')
+# for i in [12, 15, 19]:
+#     cq.enqueue(i)
+# print('check size & capacity :', cq.check_size(), '&', cq.check_capacity())
+# cq.print_queue()
+# cq.check_front_end_index()
+
