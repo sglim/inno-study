d9d6299b1d67db5f148676720d5f7a88273ad582
INO <inoh.jung@gmail.com>
2017-04-04 10:21:43 +0900

    prob. 121 best time to buy and sell stock (first solution)

--- leetcode/ino/prob_121/prob_121_best_time_to_buy_and_sell_stock(1).py
+++ leetcode/ino/prob_121/prob_121_best_time_to_buy_and_sell_stock(1).py
@@ -14,16 +14,15 @@ class Solution(object):
         profit = prices[max_idx] - prices[min_idx]
 
         for idx in range(1, len(prices)):
-            if prices[idx] > prices[max_idx]:
+            if profit < (prices[idx] - prices[min_candidate_idx]):
                 max_idx = idx
-                if profit < (prices[max_idx] - prices[min_candidate_idx]):
-                    min_idx = min_candidate_idx
-
+                min_idx = min_candidate_idx
                 profit = prices[max_idx] - prices[min_idx]
-                print('*** :', profit)
 
-            if prices[idx] < prices[min_idx]:
+            if prices[idx] < prices[min_candidate_idx]:
                 min_candidate_idx = idx
+
+            print('* cur idx :', idx, '/ min idx :', min_idx, '/ max idx :', max_idx, '/ min_candidate :', min_candidate_idx, '/ profit :', profit)
 
         if profit < 0:
             profit = 0
@@ -32,5 +31,9 @@ class Solution(object):
 
 
 sol = Solution()
-test_arr = [2,1,2,1,0,1,2]
+# test_arr = [3, 3, 5, 0, 0, 3, 1, 4]  # 4
+# test_arr = [7, 6, 4, 3, 1]  # 0
+# test_arr = [2, 1, 2, 1, 0, 1, 2]  # 2
+# test_arr = [7, 1, 5, 3, 6, 4]  # 5
+test_arr = [4, 7, 1, 2, 11]  # 10
 print(sol.maxProfit(test_arr))
