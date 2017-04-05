bc5a7f514345e40b5aa8c8821824b0202167ef43
INO <inoh.jung@gmail.com>
2017-04-05 11:59:49 +0900

    prob. 104 additional solution by stack (function - depth_stack())

--- leetcode/ino/prob_104/prob_104_maximum_depth_of_binary_tree(2).py
+++ leetcode/ino/prob_104/prob_104_maximum_depth_of_binary_tree(2).py
@@ -15,3 +15,22 @@ class Solution(object):
             return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    def depth_stack(self, root):
+        tree_stack = []
+        max_depth = 0
+
+        tree_stack.append((root, 1))
+
+        while tree_stack.empty() is not True:
+            cur_node, cur_depth = tree_stack.pop()
+
+            if cur_node.left is not None:
+                tree_stack.append((cur_node.left, cur_depth + 1))
+
+            if cur_node.right is not None:
+                tree_stack.append((cur_node.right, cur_depth + 1))
+
+            max_depth = max(max_depth, cur_depth)
+
+        return max_depth
